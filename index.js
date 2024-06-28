51f980135c557ca8ba10668cfc68ee0eb6b31fbb
Ben <bc@bencarterconsulting.com>
2024-06-28 10:08:22 -0400

    Assignment completed.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let scuberGreetingForFeet; 
+  if (rideDistance <= 400) {
+    scuberGreetingForFeet = "This one is on me!";
+  } else if (rideDistance < 2001) {
+    scuberGreetingForFeet = "That will be twenty bucks.";
+  } else if (rideDistance > 2500) {
+    scuberGreetingForFeet = "No can do.";
+  } else {
+    scuberGreetingForFeet = "I will gladly take your thirty bucks.";
+  }
+  return scuberGreetingForFeet;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  let city = "NYC";
+  let ternaryCheckCity;
+  if (destination === "NYC") {
+    ternaryCheckCity = "Ok, sounds good.";
+  } else {
+    ternaryCheckCity = "No go.";
+  }
+  return ternaryCheckCity;
 }
+function switchOnCharmFromTip(tip){
+  let switchOnCharmFromTip;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous") {
+    switchOnCharmFromTip = "Thank you so much.";
+  } else if (tip === "not as generous") {
+    switchOnCharmFromTip = "Thank you.";
+  } else {
+    switchOnCharmFromTip = "Bye.";
+  }
+
+  return switchOnCharmFromTip;
 }
